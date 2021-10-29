9c5980265b6b6876b729776980cc92f70917dd78
kalilventura <kalilventur@hotmail.com>
2021-10-29 19:14:21 -0300

    Initial project

---
+++ specification-api/src/main/java/br/com/github/kalilventura/specificationapi/SpecificationApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.github.kalilventura.specificationapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpecificationApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpecificationApiApplication.class, args);
+	}
+
+}
